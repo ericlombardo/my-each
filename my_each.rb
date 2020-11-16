903f722e03ca91e3635d9bfc5bda7358e13594d3
Eric <eric.m.lombardo@gmail.com>
2020-11-16 15:08:06 -0500

    commit tree 7bd80707eae6ca30f33f52f2dda2af53a52974b1

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)                # put argument(s) here
+  if block_given?                 # check for block
+    i = 0                         # set counter
+    while i < array.length        # while statment for argument length
+      yield array[i]              # yield array item using counter
+      i += 1                      # increment counter by 1
+    end
+  else
+    "Sorry, no block was given."  # message give when no block
+  end 
+  array                           # return original array
 end
